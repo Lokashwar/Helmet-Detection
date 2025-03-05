4b962283087b815f4defbbce77884e3df00d1a77
Lokashwar <lokashwar@gmail.com>
2025-03-05 21:37:40 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'HelmetDetection',
+    version= '0.0.0',
+    author= 'Loki',
+    author_email= 'lokashwar@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
